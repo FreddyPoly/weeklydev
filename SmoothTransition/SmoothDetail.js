5fdb0ca876e6c60eb02d49cfb342f9876978c1ed
FreddyPoly <frederic@wecraftapps.com>
2018-10-13 18:25:38 +0200

    Anim fade on enter

--- SmoothTransition/SmoothDetail.js
+++ SmoothTransition/SmoothDetail.js
@@ -1,17 +1,35 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Animated} from 'react-native';
 
 export default class SmoothDetail extends Component {
+  opacity = new Animated.Value(0);
+
+  componentWillMount = () => {
+    Animated.timing(
+      this.opacity,
+      {
+        toValue: 1,
+        duration: 550,
+      }
+    ).start();
+  }
+
   render() {
     return (
       <View style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
         backgroundColor: this.props.navigation.state.params.data.color,
       }}>
-        <Text>DETAILS</Text>
-        <Text>{ this.props.navigation.state.params.data.label }</Text>
+        <Animated.View style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: this.props.navigation.state.params.data.color,
+          opacity: this.opacity,
+        }}>
+          <Text>DETAILS</Text>
+          <Text>{ this.props.navigation.state.params.data.label }</Text>
+        </Animated.View>
       </View>
     );
   }
