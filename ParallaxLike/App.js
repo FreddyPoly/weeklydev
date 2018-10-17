a6f84f0faad43036b49788c79843c6bceee979a4
FreddyPoly <frederic@wecraftapps.com>
2018-10-17 19:39:44 +0200

    Sens du scroll

--- ParallaxLike/App.js
+++ ParallaxLike/App.js
@@ -1,24 +1,51 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Dimensions, Animated} from 'react-native';
 
 import {IndicatorViewPager} from 'rn-viewpager';
 
 export default class App extends Component {
+  posMainBeachTop = new Animated.Value((Dimensions.get('window').height / 2) - 150);
+  posMainBeachLeft = new Animated.Value((Dimensions.get('window').width / 2) - 150);
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lastOffset: 0,
+    }
+  }
+
+  _handleSwipe = async ({offset, position }) => {
+    // Cas initial pareil pour tous
+    if (offset === 0) {
+      await this.setState({lastOffset: offset});
+      return;
+    }
+
+    let swipeRight = false;
+
+    if (offset > this.state.lastOffset) {
+      swipeRight = true;
+      await this.setState({lastOffset: offset});
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <IndicatorViewPager
-          style={{flex: 1}} >
+          style={{flex: 1}}
+          onPageScroll={this._handleSwipe} >
           <View style={{
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-            <Image
+            <Animated.Image
               style={{
                 position: 'absolute',
-                top: (Dimensions.get('window').height / 2) - 150,
-                left: (Dimensions.get('window').width / 2) - 150,
+                top: this.posMainBeachTop,
+                left: this.posMainBeachLeft,
                 width: 300,
                 height: 300,
               }}
