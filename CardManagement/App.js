4088bc3746e0b738b5c35a5e3a4321dc43b4e167
FreddyPoly <frederic@wecraftapps.com>
2018-11-12 19:52:37 +0100

    Infos textuelles

--- CardManagement/App.js
+++ CardManagement/App.js
@@ -2,6 +2,41 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'WALLETS',
+      subtitle: 'Check your earnings',
+      item: [{
+        title: 'Paiement 1',
+        subtitle: 'Salaire',
+        total: 300,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
+        icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+      }, {
+        title: 'Paiement 2',
+        subtitle: 'Drogue',
+        total: 10300,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
+        icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+      }, {
+        title: 'Paiement 3',
+        subtitle: 'Loyer',
+        total: 80,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
+        icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+      }, {
+        title: 'Paiement 4',
+        subtitle: 'Poney aquatique',
+        total: 666,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
+        icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+      }],
+      burgerMenu: 'https://image.flaticon.com/icons/png/128/889/889693.png',
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
