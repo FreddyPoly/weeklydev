e930d73384da73517136248ebf1d2c6a21896858
FreddyPoly <frederic@wecraftapps.com>
2018-11-14 16:51:20 +0100

    présentation

--- CardManagement/App.js
+++ CardManagement/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView, Dimensions} from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
@@ -14,33 +14,56 @@ export default class App extends Component {
         total: 300,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+        color: '#F79131',
       }, {
         title: 'Paiement 2',
         subtitle: 'Drogue',
         total: 10300,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+        color: '#009EDE',
       }, {
         title: 'Paiement 3',
         subtitle: 'Loyer',
         total: 80,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+        color: '#00AC65',
       }, {
         title: 'Paiement 4',
         subtitle: 'Poney aquatique',
         total: 666,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
+        color: '#765BA7',
       }],
-      burgerMenu: 'https://image.flaticon.com/icons/png/128/889/889693.png',
     }
   }
 
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>MAIN</Text>
+        <ScrollView style = {{ flex: 1 }}>
+          {this.state.item.map((item) => (
+            <View
+              style={{
+                width: Dimensions.get('window').width * .8,
+                height: 200,
+                backgroundColor: item.color,
+                marginTop: 24,
+                marginBottom: 24,
+                borderRadius: 8,
+                padding: 16
+              }}>
+              <Text style={{color: 'white', fontSize: 28, fontWeight: '700'}}>{ item.title }</Text>
+              <Text style={{color: 'white', fontSize: 20}}>{ item.subtitle }</Text>
+
+              <View style={{flex: 1, justifyContent: 'flex-end'}}>
+                <Text style={{color: 'white', fontSize: 36, fontWeight: '700', textAlign: 'right'}}>{ item.total }€</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     );
   }
