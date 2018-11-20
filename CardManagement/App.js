f561c74444c9ceb85135efda242b06f920f2952d
FreddyPoly <frederic@wecraftapps.com>
2018-11-20 22:38:51 +0100

    Disparition cards

--- CardManagement/App.js
+++ CardManagement/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, ScrollView, Dimensions, TouchableWithoutFeedback, Animated} from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView, Dimensions, TouchableOpacity, Animated} from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
@@ -15,7 +15,6 @@ export default class App extends Component {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
         color: '#F79131',
-        hidden: false,
       }, {
         title: 'Paiement 2',
         subtitle: 'Drogue',
@@ -23,7 +22,6 @@ export default class App extends Component {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
         color: '#009EDE',
-        hidden: false,
       }, {
         title: 'Paiement 3',
         subtitle: 'Loyer',
@@ -31,7 +29,6 @@ export default class App extends Component {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
         color: '#00AC65',
-        hidden: false,
       }, {
         title: 'Paiement 4',
         subtitle: 'Poney aquatique',
@@ -39,19 +36,21 @@ export default class App extends Component {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis sed dui in semper. Ut ornare placerat malesuada. Aliquam erat volutpat. Maecenas vitae magna accumsan, vestibulum lorem ut, tristique leo. Donec fermentum pharetra nibh, sit amet suscipit ipsum condimentum at. Proin non mollis lacus, vel fringilla odio. Curabitur in porta urna. In sed sapien quis lorem tincidunt dictum. Suspendisse potenti.',
         icon: 'https://image.flaticon.com/icons/png/128/138/138292.png',
         color: '#765BA7',
-        hidden: false,
       }],
     }
   }
 
-  _hideCards = (title) => {
-    const newList = this.state.item.map(e => e.title !== title ? { ...e, hidden: true } : e);
-
-    this.setState({item: newList});
-  }
+  opacityCards = new Animated.Value(1);
 
   _selectCard = (item) => {
-    this._hideCards(item.title);
+    // Hide other cards
+    Animated.timing(
+      this.opacityCards,
+      {
+        toValue: 0,
+        duration: 200,
+      }
+    ).start();
   }
 
   render() {
@@ -59,29 +58,28 @@ export default class App extends Component {
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ScrollView style = {{ flex: 1 }} showsVerticalScrollIndicator={false}>
           {this.state.item.map((item) => (
-            !item.hidden ?
-              <TouchableWithoutFeedback
-                onPress={() => this._selectCard(item)}>
-                <Animated.View
-                  style={{
-                    width: Dimensions.get('window').width * .8,
-                    height: 200,
-                    backgroundColor: item.color,
-                    marginTop: 24,
-                    marginBottom: 24,
-                    borderRadius: 8,
-                    padding: 16
-                  }}>
-                  <Text style={{color: 'white', fontSize: 28, fontWeight: '700'}}>{ item.title }</Text>
-                  <Text style={{color: 'white', fontSize: 20}}>{ item.subtitle }</Text>
+            <TouchableOpacity
+              key={item.title}
+              onPress={() => this._selectCard(item)}>
+              <Animated.View
+                style={{
+                  width: Dimensions.get('window').width * .8,
+                  height: 200,
+                  backgroundColor: item.color,
+                  marginTop: 24,
+                  marginBottom: 24,
+                  borderRadius: 8,
+                  padding: 16,
+                  opacity: this.opacityCards,
+                }}>
+                <Text style={{color: 'white', fontSize: 28, fontWeight: '700'}}>{ item.title }</Text>
+                <Text style={{color: 'white', fontSize: 20}}>{ item.subtitle }</Text>
 
-                  <View style={{flex: 1, justifyContent: 'flex-end'}}>
-                    <Text style={{color: 'white', fontSize: 36, fontWeight: '700', textAlign: 'right'}}>{ item.total }€</Text>
-                  </View>
-                </Animated.View>
-              </TouchableWithoutFeedback>
-            :
-              null
+                <View style={{flex: 1, justifyContent: 'flex-end'}}>
+                  <Text style={{color: 'white', fontSize: 36, fontWeight: '700', textAlign: 'right'}}>{ item.total }€</Text>
+                </View>
+              </Animated.View>
+            </TouchableOpacity>
           ))}
         </ScrollView>
       </View>
